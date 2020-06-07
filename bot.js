6720ca62362882622fcc9aa322dc8084a42a5219
slesolliec <slesolliec@gmail.com>
2020-06-07 18:18:27 +0200

    list guilds and members

--- bot.js
+++ bot.js
@@ -5,18 +5,50 @@ const conf    = require('./configs');
 
 
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
+
+    // loop on my guilds
+    client.guilds.cache.forEach(processOneGuild);
 });
 
-client.login(conf.botToken);
 
+// take a guild: list all its members
+function processOneGuild(guild) {
+    console.log("=== " + guild.name + " (" + guild.memberCount + " membres) ===");
+    guild.members.fetch()
+    .then(displayMembers)
+    .catch(console.error)
+}
+
+// loop on member map
+function displayMembers(memberMap) {
+    memberMap.forEach(displayMember)
+}
+
+// display one member
+function displayMember(member) {
+    console.log(member.user.username + " (" + member.user.id + ")");
+}
+
+
+
+
+// chat bot basic loop
 client.on('message', message => {
-//    console.log(message.content);
-    console.log(message);
+    // console.log(message.content);
+    // console.log(message);
     
+    message.channel.guild.members.fetch()
+        .then(displayMembers)
+        .catch(console.error)
+    
+/*
     if (! message.author.bot) {
         message.channel.send("je répond au message de " + message.author.username)
     }
+*/
 
 });
 
+
+client.login(conf.botToken);
