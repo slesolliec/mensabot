41b32a4c5509e58e947e06265faadf92211385af
slesolliec <slesolliec@gmail.com>
2020-06-08 12:17:44 +0200

    add guilds and members to db

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const db      = require('./mods/db');
 
 
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log('Bot is connected to Discord');
 
     // loop on my guilds
     client.guilds.cache.forEach(processOneGuild);
@@ -16,9 +16,20 @@ client.once('ready', () => {
 // take a guild: list all its members
 function processOneGuild(guild) {
     console.log("=== " + guild.name + " (" + guild.memberCount + " membres) ===");
+
+    // check we know that guild
+    db.get("select * from guilds where gid = ?", guild.id, (err, row) => {
+        if (err) return console.log(err.message);
+
+        if (row) return;
+        
+        db.run("insert into guilds(gid, name) values(?, ?)", [guild.id, guild.name], handlerr);
+    })
+
+    // we get the list of members of that guild
     guild.members.fetch()
-    .then(processAllMembers)
-    .catch(console.error)
+        .then(processAllMembers)
+        .catch(console.error)
 }
 
 // loop on member map
@@ -28,26 +39,39 @@ function processAllMembers(memberMap) {
 
 // check that this member is inside our database
 function checkMember(member) {
+    // check we know that user
     db.get("select * from users where did = ?", member.user.id, (err, row) => {
+        if (err) return console.error(err.message);
+
+        if (! row) {
+            // member is not here
+            db.run("insert into users(did, discord_name) values(?, ?)", [member.user.id, member.user.username], handlerr)
+        }
+    });
+
+    // check we have the user / guild relationship
+    db.get("select * from members where gid = ? and did = ?", [member.guild.id, member.user.id], (err, row) => {
         if (err) {
           return console.error(err.message);
         }
 
         if (! row) {
             // member is not here
-            db.run("insert into users(did, discord_name) values(?, ?)", [member.user.id, member.user.username], function(err) {
-                if (err) {
-                  return console.log(err.message);
-                }
-                // get the last insert id
-                console.log(`User has been inserted with rowid ${this.lastID}`);
-              })
+            db.run("insert into members(gid, did) values(?, ?)", [member.guild.id, member.user.id], handlerr)
         }
       
-    });
+    })
+
     console.log(member.user.username + " (" + member.user.id + ")");
 }
 
+
+// handle errors
+function handlerr(err) {
+    if (err) {
+        console.error(err.message);
+    }
+}
 
 
 
--- db/db.sql
+++ db/db.sql
@@ -6,5 +6,28 @@ CREATE TABLE users (
     discord_name STRING (0, 100),
     firstname    STRING (0, 200),
     lastname     STRING (0, 200),
-    email        STRING (0, 100) UNIQUE
+    email        STRING (0, 100) UNIQUE,
+    state        STRING (0, 20)  DEFAULT new
+);
+
+
+-- did = Discord ID
+-- mid = numéro Mensa
+-- state =
+--     new : nouveau contact
+--     welcomed : le message de bienvenue lui a été envoyé
+--     validationCodeSent : le code de validation lui a été envoyé
+--     validated : utilisteur validé en tant que Mensan
+
+
+-- servers
+CREATE TABLE guilds (
+    gid  INTEGER         PRIMARY KEY,
+    name STRING (0, 200) 
+);
+
+-- memberships: which users belong to which guilds
+CREATE TABLE members (
+    gid INTEGER NOT NULL,
+    did INTEGER NOT NULL
 );
