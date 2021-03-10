252e7804373f6d31f131d20027d9ecac3bcb2693
slesolliec <slesolliec@gmail.com>
2021-03-10 22:12:34 +0100

    put a date on mysql dump file

--- dump_db.js
+++ dump_db.js
@@ -1,5 +1,6 @@
-const util = require('util');
-const conf = require('./configs');
+const util   = require('util');
+const conf   = require('./configs');
+const moment = require('moment');
 
 const exec = util.promisify(require('child_process').exec);
 
@@ -7,7 +8,9 @@ const exec = util.promisify(require('child_process').exec);
 
 async function dump_db() {
 	try {
-		const cmd = 'mysqldump -u ' + conf.mysql.user + ' -p' + conf.mysql.password + ' --no-tablespaces ' + conf.mysql.database + ' > mensabot_dump.sql';
+		const cmd = 'mysqldump -u ' + conf.mysql.user + ' -p' + conf.mysql.password
+			+ ' --no-tablespaces ' + conf.mysql.database
+			+ ' > mensabot_dump_' + moment().format('YYYY-MM-DD') + '.sql';
 		const { stdout, stderr } = await exec(cmd);
 		console.log('stdout:', stdout);
 		console.log('stderr:', stderr);
