a00c6732488038f94847f4d64399cb483101c29b
slesolliec <slesolliec@gmail.com>
2021-04-17 13:59:05 +0200

    login button

---
+++ components/LoginButton.vue
@@ -0,0 +1,15 @@
+<template>
+	<button @click="login">Login</button>
+</template>
+
+<script>
+export default {
+    methods: {
+        login() {
+            this.$auth.loginWith('discord', { params: { response_type: 'token' } })
+              .then(() => this.$toast.success('Logged In!'))
+        },
+    }
+}
+</script>
+
