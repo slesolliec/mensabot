310f5f9e23004c882e331bdbea11a14f99451774
slesolliec <slesolliec@gmail.com>
2021-04-19 23:02:29 +0200

    no more genrage

---
+++ db/some_queries.sql
@@ -0,0 +1,4 @@
+update users set real_name = replace(real_name, 'Monsieur ', '');
+update users set real_name = replace(real_name, 'Madame ', '');
+update users set real_name = replace(real_name, 'Mademoiselle ', '');
+
