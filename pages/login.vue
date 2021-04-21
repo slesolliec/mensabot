d395fa75c1826f093e3a7989928b4dc257f5f368
slesolliec <slesolliec@gmail.com>
2021-04-21 13:28:15 +0200

    add login + logout pages

---
+++ pages/login.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div>
+    <h1>Connexion</h1>
+
+	<p>Ce site est un site privé.</p>
+	
+	<p>Pour accéder à son contenu, vous pouvez vous authentifier via Discord en cliquant sur le bouton en haut à droite.</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  auth: guest
+}
+// https://discord.com/oauth2/authorize?protocol=oauth2&response_type=token&access_type&client_id=719154428355018813&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&scope=identify%20email%20identify&state=Fg4hk3hzFQ&code_challenge_method=implicit&nonce=I956bFqWXH
+</script>
---
+++ pages/logout.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div>
+    <h1>Connexion</h1>
+
+	<p>Ce site est un site privé.</p>
+	
+	<p>Pour accéder à son contenu, vous pouvez vous authentifier via Discord en cliquant sur le bouton en haut à droite.</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  auth: false
+}
+</script>
