a70d750a51eff9113b1a3f3ae423a15049100c1c
slesolliec <slesolliec@gmail.com>
2021-04-28 22:54:36 +0200

    add redirect to https

---
+++ redirect443.js
@@ -0,0 +1,13 @@
+// set up plain http server
+var http = express();
+
+// set up a route to redirect http to https
+http.get('*', function(req, res) {  
+    res.redirect('https://' + req.headers.host + req.url);
+
+    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+    // res.redirect('https://example.com' + req.url);
+})
+
+// have it listen on 80
+http.listen(80);
