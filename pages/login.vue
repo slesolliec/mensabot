03cb13148ab4f3551dc7c7b801fcad80f07dbe31
Stéphane Le Solliec <slesolliec@gmail.com>
2021-11-06 13:39:44 +0100

    bugfix: site would crash for unlogged users

--- pages/login.vue
+++ pages/login.vue
@@ -12,7 +12,7 @@
 
 <script>
 export default {
-  auth: guest
+  auth: 'guest'
 }
 // https://discord.com/oauth2/authorize?protocol=oauth2&response_type=token&access_type&client_id=719154428355018813&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&scope=identify%20email%20identify&state=Fg4hk3hzFQ&code_challenge_method=implicit&nonce=I956bFqWXH
 </script>
